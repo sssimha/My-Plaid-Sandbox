e4f8ac7a328d977c77e47c683ff77f862d3eb1e9
sssimha <sairam.simha@gmail.com>
2017-02-20 08:39:38 -0600

    Update referencing actions
    Update referencing actions

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from PlaidSandbox import FlaskTest
-from PlaidSandbox import getCredentials
-from PlaidSandbox import test1
-from PlaidSandbox import UserFunctions
+import .FlaskTest
+import .getCredentials
+import .test1
+import .UserFunctions
